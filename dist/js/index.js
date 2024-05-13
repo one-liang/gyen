425f43c60353edd0093ce23c1318928b1ad337d8
one-liang <william810212@gmail.com>
2024-05-14 01:19:55 +0800

    chore: add header menu

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,143 @@
-import './gotop';
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ (function() {
 
-const a = 111;
-console.log(a);
+// gotop
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+document.addEventListener("DOMContentLoaded", function () {
+  var goTopButton = document.querySelector("#gotop");
+  var windowViewPortHeight = window.innerHeight;
+  var isRequestingAnimationFrame = false;
+  if (!goTopButton) {
+    return;
+  }
+  goTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  window.addEventListener("scroll", function () {
+    if (!isRequestingAnimationFrame) {
+      requestAnimationFrame(filterGoTopButtonVisibility);
+      isRequestingAnimationFrame = true;
+    }
+  });
+  function filterGoTopButtonVisibility() {
+    var windowPageYOffset = window.pageYOffset || document.documentElement.scrollTop;
+    if (windowPageYOffset > windowViewPortHeight) {
+      goTopButton.classList.add("show");
+      isRequestingAnimationFrame = false;
+    } else {
+      goTopButton.classList.remove("show");
+      requestAnimationFrame(filterGoTopButtonVisibility);
+    }
+  }
+});
+// gotop end
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+!function() {
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _gotop__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _gotop__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_gotop__WEBPACK_IMPORTED_MODULE_0__);
+
+var navMenu = document.querySelectorAll("#navMenu button");
+var menuList = document.querySelector("#menuList");
+var menuListItem = document.querySelector("#menuListItem");
+console.log("navMenu", navMenu);
+navMenu.forEach(function (nav) {
+  nav.addEventListener("click", function (event) {
+    var navText = nav.textContent;
+
+    // 切換 menuList 顯示狀態
+    menuList.style.display = menuList.style.display === "none" ? "block" : "none";
+    var links = menuListItem.querySelectorAll("a");
+
+    // 為連結加 'border-black' class
+    links.forEach(function (link) {
+      console.log(link.textContent);
+      if (navText === link.textContent) {
+        link.classList.add("!border-black");
+      } else {
+        link.classList.remove("!border-black");
+      }
+    });
+  });
+});
+}();
+/******/ })()
+;
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
-import './gotop';
+import "./gotop";
 
-const a = 111;
-console.log(a);
+const navMenu = document.querySelectorAll("#navMenu button");
+const menuList = document.querySelector("#menuList");
+const menuListItem = document.querySelector("#menuListItem");
+console.log("navMenu", navMenu);
+navMenu.forEach((nav) => {
+  nav.addEventListener("click", (event) => {
+    const navText = nav.textContent;
+
+    // 切換 menuList 顯示狀態
+    menuList.style.display =
+      menuList.style.display === "none" ? "block" : "none";
+
+    const links = menuListItem.querySelectorAll("a");
+
+    // 為連結加 'border-black' class
+    links.forEach((link) => {
+      console.log(link.textContent);
+      if (navText === link.textContent) {
+        link.classList.add("!border-black");
+      } else {
+        link.classList.remove("!border-black");
+      }
+    });
+  });
+});
