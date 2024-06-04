f7dc8f9eddcc95c8a2b85b119153a83b7bcdfae0
one-liang <william810212@gmail.com>
2024-06-05 00:58:43 +0800

    chore: add product swiper

--- src/js/products.js
+++ src/js/products.js
@@ -1,4 +1,27 @@
 import "./header";
 import "./gotop";
 
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  // 商品輪播小圖
+  const swiperProductsThumb = new Swiper(".swiper-products-thumb", {
+    spaceBetween: 20,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".swiper-button-products-thumb-next",
+      prevEl: ".swiper-button-products-thumb-prev",
+    },
+  });
+
+  // 商品輪播
+  const swiperProducts = new Swiper(".swiper-products", {
+    thumbs: {
+      swiper: swiperProductsThumb,
+    },
+    navigation: {
+      nextEl: ".swiper-button-products-next",
+      prevEl: ".swiper-button-products-prev",
+    },
+  });
+});
