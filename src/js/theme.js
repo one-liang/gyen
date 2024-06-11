f373af62426a47d0d1496cf759a2597be72a0c1b
one-liang <william810212@gmail.com>
2024-06-12 00:05:30 +0800

    chore: update theme mobile layout

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,22 +2,29 @@ import "./header";
 import "./gotop";
 
 document.addEventListener("DOMContentLoaded", function () {
-  // 猜你會喜歡
-  new Swiper(".swiper-alsoLike", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    freeMode: true,
-    breakpoints: {
-      // when window width is >= 1024px
-      1024: {
-        freeMode: false,
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-    },
-    navigation: {
-      nextEl: ".swiper-button-alsoLike-next",
-      prevEl: ".swiper-button-alsoLike-prev",
-    },
-  });
+  let swiperInstance;
+
+  function initializeSwiper() {
+    if (window.innerWidth >= 1024) {
+      if (!swiperInstance) {
+        // 猜你會喜歡
+        swiperInstance = new Swiper(".swiper-alsoLike", {
+          freeMode: false,
+          slidesPerView: 4,
+          spaceBetween: 40,
+          navigation: {
+            nextEl: ".swiper-button-alsoLike-next",
+            prevEl: ".swiper-button-alsoLike-prev",
+          },
+        });
+      }
+    } else {
+      if (swiperInstance) {
+        swiperInstance.destroy(true, true);
+        swiperInstance = null;
+      }
+    }
+  }
+
+  window.addEventListener("resize", initializeSwiper);
 });
