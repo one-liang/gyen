357acc10babc9774a9869fd67ec97e4ee877254f
one-liang <william810212@gmail.com>
2024-06-11 23:35:36 +0800

    chore: add tagList expand & collapse logic

--- src/js/category-pagination.js
+++ src/js/category-pagination.js
@@ -1,4 +1,22 @@
 import "./header";
 import "./gotop";
 
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth < 1024) {
+    const expandBtn = document.querySelector("#expandBtn");
+    const collapseBtn = document.querySelector("#collapseBtn");
+    const tagList = document.querySelector("#tagList");
+
+    expandBtn.addEventListener("click", function () {
+      tagList.classList.add("!max-h-fit");
+      expandBtn.classList.add("!hidden");
+      collapseBtn.classList.remove("!hidden");
+    });
+
+    collapseBtn.addEventListener("click", function () {
+      tagList.classList.remove("!max-h-fit");
+      expandBtn.classList.remove("!hidden");
+      collapseBtn.classList.add("!hidden");
+    });
+  }
+});
